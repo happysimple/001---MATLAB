92620fe08038cd747e28130d4e8eff15c07dda05
simplehappy <1292584987@qq.com>
2022-06-07 11:55:27 +0800

    first commit

---
+++ build/html/_sources/index.md.txt
@@ -0,0 +1,10 @@
+# Matlab
+
+```{toctree}
+:maxdepth: 2
+
+001---科研绘图/index
+002---数据处理/index
+003---Have Fun/index
+004---其他/index
+```
---
+++ source/conf.py
@@ -0,0 +1,31 @@
+# -- Project configuration -----------------------------------------------------
+project = 'Matlab'
+author = '王开心'
+copyright = '2022, 王开心'
+release = '1.0.0'
+
+# -- General configuration ------------------------------------------------
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+extensions = [
+    'myst_parser',            # Markdown
+    'sphinx.ext.mathjax',     # Math
+    'sphinx_copybutton',      # Copy Code
+    'sphinx.ext.githubpages', # Gighub
+    'sphinx_panels',          # panels
+]
+# mathjax_path = "https://cdn.bootcss.com/mathjax/2.7.7/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+
+# -- Options for HTML output ----------------------------------------------
+templates_path = ['_templates']
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_theme_options = {
+    "prev_next_buttons_location": "bottom",
+}
+
+
